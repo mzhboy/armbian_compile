262971e74c04e64393666d234e61f4cd73f0bb1b
may <mzhboy@gmail.com>
2019-05-06 12:22:59 +0800

    add new script for faster compiling, only make clean images

---
+++ proxy_compile_clean_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo find ./build/cache/toolchains/ -name *.aria2 -type f -delete
+sudo find ./build/cache/toolchains/ -name *.tar.xz -type f -delete
+sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
+cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
+env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
+    BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 \
+    BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works CLEAN_LEVEL=images
+# branch works
