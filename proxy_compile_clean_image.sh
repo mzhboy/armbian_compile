05caa223501c0927c50de3ba680d681650180538
may <mzhboy@gmail.com>
2019-05-06 12:39:57 +0800

    add more options

--- proxy_compile_clean_image.sh
+++ proxy_compile_clean_image.sh
@@ -5,5 +5,5 @@ sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
     BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 \
-    BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works CLEAN_LEVEL=images
+    BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works CLEAN_LEVEL=images IGNORE_UPDATES=yes FORCE_BOOTSCRIPT_UPDATE=no
 # branch works
