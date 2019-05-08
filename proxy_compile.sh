87d8e04625b0ea068ade282aba09723f84ce9d9a
may <mzhboy@gmail.com>
2019-05-08 15:17:23 +0800

    [git] proxy target change to git.kernel.org

--- proxy_compile.sh
+++ proxy_compile.sh
@@ -3,7 +3,8 @@ sudo find ./build/cache/toolchains/ -name *.aria2 -type f -delete
 sudo find ./build/cache/toolchains/ -name *.tar.xz -type f -delete
 sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
 
-sudo git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
+#sudo git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
+sudo git config --global http.https://git.kernel.org.proxy socks5h://192.168.1.16:1081
 
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
