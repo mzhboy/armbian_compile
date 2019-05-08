95b1f8e23a0483c3b854daa56503e417452d160e
may <mzhboy@gmail.com>
2019-05-08 14:47:04 +0800

    fix sudo

--- proxy_compile_clean_image.sh
+++ proxy_compile_clean_image.sh
@@ -3,7 +3,7 @@ sudo find ./build/cache/toolchains/ -name *.aria2 -type f -delete
 sudo find ./build/cache/toolchains/ -name *.tar.xz -type f -delete
 sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
 
-git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
+sudo git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
 
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
@@ -11,4 +11,4 @@ env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNE
     CLEAN_LEVEL=images IGNORE_UPDATES=yes FORCE_BOOTSCRIPT_UPDATE=no FORCE_CHECKOUT=yes
 # branch works
 
-# git config --global --unset http.https://github.com.proxy
+# sudo git config --global --unset http.https://github.com.proxy
