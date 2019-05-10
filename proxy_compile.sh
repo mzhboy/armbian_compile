157439b3add4e5c6a158c2f763631b3bfef66008
may <mzhboy@gmail.com>
2019-05-10 12:41:50 +0800

    add options

--- proxy_compile.sh
+++ proxy_compile.sh
@@ -3,14 +3,11 @@ sudo find ./build/cache/toolchains/ -name *.aria2 -type f -delete
 sudo find ./build/cache/toolchains/ -name *.tar.xz -type f -delete
 sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
 
-#sudo git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
-sudo git config --global http.https://git.kernel.org.proxy socks5h://192.168.1.16:1081
-
+git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
-    BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 \
+    BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 BTRFS_COMPRESSION=zstd DOWNLOAD_MIRROR=china \
     BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works 
 # branch works
 
-# sudo git config --global --unset http.https://github.com.proxy
-
+# git config --global --unset http.https://github.com.proxy
--- proxy_compile_clean_image.sh
+++ proxy_compile_clean_image.sh
@@ -9,6 +9,7 @@ sudo git config --global http.https://git.kernel.org.proxy socks5h://192.168.1.1
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
     BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works \
+    BTRFS_COMPRESSION=zstd DOWNLOAD_MIRROR=china \
     CLEAN_LEVEL=images IGNORE_UPDATES=yes FORCE_BOOTSCRIPT_UPDATE=no FORCE_CHECKOUT=yes
 # branch works
 
