8af934a1a4b03bb96b21970b91f705579dfd136a
may <mzhboy@gmail.com>
2019-05-22 15:54:57 +0800

    release=disco

--- proxy_compile.sh
+++ proxy_compile.sh
@@ -6,7 +6,7 @@ sudo find ./cache/toolchains/ -type f -exec ln {} ./build/cache/toolchains/ \;
 git config --global http.https://github.com.proxy socks5h://192.168.1.16:1081
 cd "$(dirname "$(find /home/mei/ -name 'compile.sh' -type f)")" && sudo git checkout master || exit 1
 env ALL_PROXY=socks5h://192.168.1.16:1081 ./compile.sh KERNEL_CONFIGURE=no KERNEL_KEEP_CONFIG=no BUILD_DESKTOP=no \
-    BRANCH=dev RELEASE=stretch KERNEL_ONLY=no BOARD=orangepilite2 BTRFS_COMPRESSION=zstd DOWNLOAD_MIRROR=china \
+    BRANCH=dev RELEASE=disco KERNEL_ONLY=no BOARD=orangepilite2 BTRFS_COMPRESSION=zstd DOWNLOAD_MIRROR=china \
     BUILD_KSRC=no ROOTFS_TYPE=btrfs LIB_TAG=works 
 # branch works
 
